dd9b7cc9572f59c15c9bf59bdbc8ceaa603461f2
ota200 <aniscratch@gmail.com>
2022-07-18 21:12:31 -0400

    Drag+ start of notes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 //import Drag from './drag'
-import React, { useState } from 'react';
-
+import React, { useEffect,useState } from 'react';
+import './App.css'
 
 function Drag() {
   return (
-    <div >
+    <div>
       <textarea></textarea>
       <br></br>
     </div>
@@ -14,6 +14,30 @@ function Drag() {
 
 function App() {
 
+  const [coords, setCoords] = useState({x: 0, y: 0});
+
+  const [globalCoords, setGlobalCoords] = useState({x: 0, y: 0});
+
+  useEffect(() => {
+    const handleWindowMouseMove = event => {
+      setGlobalCoords({
+        x: event.screenX,
+        y: event.screenY,
+      });
+    };
+    window.addEventListener('mousemove', handleWindowMouseMove);
+
+    return () => {
+      window.removeEventListener('mousemove', handleWindowMouseMove);
+    };
+  }, []);
+
+  const handleMouseMove = event => {
+    setCoords({
+      x: event.clientX - event.target.offsetLeft,
+      y: event.clientY - event.target.offsetTop,
+    });
+  };
 
 
   const [count, setCount] = useState([]);
@@ -26,6 +50,9 @@ function App() {
       }]);
   }
 
+  function pos(e){
+    console.log(e.currentTarget.id)
+  }      
 
   const remove = (index) => () =>
 
@@ -33,12 +60,11 @@ function App() {
 
   
   return (
-    <>
-      <button onClick={add}>ADDDDDDDDDDDDD</button>
+    <div id="big">
       
       { count.map((c,index) => {
         return(
-          <div key={c.id + Math.random()}>
+          <div onClick={pos} id={"co"+ Math.random()} key={c.id + Math.random()}>
             
             {c.name}
 
@@ -48,11 +74,30 @@ function App() {
             key:{c.id + Math.random()}
           
           </div>
+
+
+
         ) 
           
       })}
+      <button id="add" onClick={add}>+</button>
 
-    </>
+      <div
+        onMouseMove={handleMouseMove}
+        style={{padding: '3rem', backgroundColor: 'lightgray'}}
+      >
+        <h2>
+          Coords: {coords.x} {coords.y}
+        </h2>
+      </div>
+
+      <hr />
+
+      <h2>
+        Global coords: {globalCoords.x} {globalCoords.y}
+      </h2>
+
+    </div>
   );
 }
 
--- src/drag.js
+++ src/drag.js
@@ -4,20 +4,9 @@ function Drag() {
     <div >
       <textarea></textarea>
       <br></br>
-      <button>Del</button>
-      <br></br>
     </div>
   );
 }
 
 export default Drag;
 
-/*
-const remove = () => {
-  setEmployees(current =>
-    current.filter(employee => {
-      return employee.id !== 3 && employee.id !== 2;
-    }),
-  );
-};
-*/
